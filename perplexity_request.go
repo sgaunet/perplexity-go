745f08226ab611fab8cd9deb085f96609dbaa9b9
Patrick Rutledge <patrick@amuseapp.com>
2025-07-31 11:43:42 -0400

    temperature should support the value 0

--- perplexity_request.go
+++ perplexity_request.go
@@ -54,7 +54,7 @@ type CompletionRequest struct {
 	// Temperatur: The amount of randomness in the response, valued between 0 inclusive and 2 exclusive.
 	// Higher values are more random, and lower values are more deterministic.
 	// Required range: 0 < x < 2
-	Temperature float64 `json:"temperature" validate:"gt=0,lt=2"`
+	Temperature float64 `json:"temperature" validate:"gte=0,lt=2"`
 	// TopP: The nucleus sampling threshold, valued between 0 and 1 inclusive.
 	// For each subsequent token, the model considers the results of the tokens with top_p probability mass.
 	// We recommend either altering top_k or top_p, but not both.
